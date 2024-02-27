a5fd728e71354e3db5c153e6b65d891d92982990
Eduardo <eduardof@fmb.unesp.br>
2024-02-26 23:24:56 -0300

    commit 15

--- src/index.ts
+++ src/index.ts
@@ -71,6 +71,32 @@ app.post("/user", async (req, res) => {
     
 });
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await prisma.user.findFirst({ where: { email: email }});
+
+  if (!user) {
+    return res.status(400).json({ message: 'e-mail/Senha inválidos' })
+  }
+
+  const verifyPass = await bcrypt.compare(password, user.password)
+
+  if (!verifyPass) {
+    return res.status(400).json({ message: 'e-mail/Senha inválidos' })
+  }
+
+  const token = jwt.sign({ id: user?.id }, process.env.JWT_PASS ?? '', {
+    expiresIn: '8h',
+  })
+
+  const { password: _, ...userLogin } = user
+
+  return res.json({
+    user: userLogin,
+    token: token,
+  })
+})
 
 app.listen(Number(port), "0.0.0.0", () => {
     console.log(`Example app listening at http://localhost:${port}`);
