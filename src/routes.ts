c368cdfb919069426375328b538fd54d140a7282
Eduardo <eduardof@fmb.unesp.br>
2024-02-27 11:55:32 -0300

    commit 104

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,7 @@ const routes = Router()
 routes.post('/user', new UserController().create)
 routes.post('/login', new UserController().login)
 
-routes.use(authMiddleware)
-routes.get('/profile', new UserController().getProfile)
-routes.get('/users', new UserController().users)
+routes.get('/profile', authMiddleware, new UserController().getProfile)
+routes.get('/users', authMiddleware, new UserController().users)
 
 export default routes
